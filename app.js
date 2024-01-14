0820dc71b8221eef73d7b680c90b5dbef24c3992
Daniela <daniela.botsu@gmail.com>
2024-01-14 18:39:09 +0200

    solved the port problem in the app.js file

--- app.js
+++ app.js
@@ -11,5 +11,6 @@ app.get('/feature-1', (req, res) => {
 })
 
 app.listen(port, () => {
-  console.log('Example app listening at http://localhost:${port}')
+  console.log('Example app listening at http://localhost:' + port)
 })
+
